1ee3b27d9ef627b36313edbfd29ed8afa69b6661
JuanDa72 <jcruzgi@unal.edu.co>
2024-07-20 21:44:35 -0500

    update group class and start schedule

--- src/com/model/group.java
+++ src/com/model/group.java
@@ -1,6 +1,7 @@
 package com.model;
 import java.time.LocalTime;
 import com.datastructures.sequential.SinglyLinkedList;
+import java.time.DayOfWeek;
 
 public class group {
 
@@ -11,7 +12,7 @@ public class group {
     private LocalTime startTime;
     private LocalTime endTime;
     private String location;
-    private String[] days;
+    private DayOfWeek[] days;
 
     public group(int number, String teacher, String faculty, int startHour, int finishHour, String location, String day) {
         this.number = number;
@@ -20,16 +21,45 @@ public class group {
         this.startTime = LocalTime.of(startHour, 0);
         this.endTime = LocalTime.of(finishHour, 0);
         this.location = location;
-        this.days = day.split("-");
+        this.days = convertDaysOfWeek(day);
+    }
+
+    private DayOfWeek[] convertDaysOfWeek(String days){
+        String [] aDays=days.split("-");
+        DayOfWeek [] dofarray = new DayOfWeek[aDays.length];
+        for(int i=0; i<aDays.length; i++){
+            if(aDays[i].equals("lunes")){
+                dofarray[i]= DayOfWeek.MONDAY;
+            }
+            else if(aDays[i].equals("martes")){
+                dofarray[i]= DayOfWeek.TUESDAY;
+            }
+            else if(aDays[i].equals("miércoles")){
+                dofarray[i]= DayOfWeek.WEDNESDAY;
+            }
+            else if(aDays[i].equals("jueves")){
+                dofarray[i]= DayOfWeek.THURSDAY;
+            }
+            else if(aDays[i].equals("viernes")){
+                dofarray[i]= DayOfWeek.FRIDAY;
+            }
+            else if(aDays[i].equals("sábado")){
+                dofarray[i]= DayOfWeek.MONDAY;
+            }
+        }
+        return dofarray;
     }
 
     public String getInformation(){
-        String days=String.join("-",this.days);
-        String information="Grupo número: "+this.number+", Profesor: "+this.teacher+", Facultad: "+this.faculty+", Hora de inicio: "+startTime.toString()+", Hora de finalización: "+endTime.toString()+", Ubicación: "+this.location+", Días: "+days;
+        int j = 0;
+        String d="";
+        for(int i=0; i<this.days.length-1;i++){
+            d+=this.days[i].toString()+"-";
+            j=i;
+        }
+        d+=this.days[j+1].toString();
+        String information="Grupo número: "+this.number+", Profesor: "+this.teacher+", Facultad: "+this.faculty+", Hora de inicio: "+startTime.toString()+", Hora de finalización: "+endTime.toString()+", Ubicación: "+this.location+", Días: "+d;
         return information;
     }
-
-
-
 
 }
--- src/com/model/schedule.java
+++ src/com/model/schedule.java
@@ -1,4 +1,27 @@
 package com.model;
+import com.datastructures.sequential.SinglyLinkedList;
+import com.model.subject;
+import java.time.DayOfWeek;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class schedule {
+
+    private SinglyLinkedList<subject> subjects=new SinglyLinkedList<>();
+
+    private Map<DayOfWeek,subject> schedule=new TreeMap<>(); //Also could be treemap with keys-value=DayOfWeek-groups
+    private String scheduleName;
+
+    public schedule(String name){
+        this.scheduleName=name;
+    }
+
+
+
+
+
+
+
+    
+
 }
